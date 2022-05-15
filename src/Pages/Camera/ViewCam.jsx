935e65404e559a6fdb2f4ce48d2b6dfc4f6d0d7b
Abdelrahman Mohamed <80596612+AbdElrahmanBEBO@users.noreply.github.com>
2022-05-15 16:13:32 +0200

    make array of screenshots and put them in array

--- src/Pages/Camera/ViewCam.jsx
+++ src/Pages/Camera/ViewCam.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import Webcam from "react-webcam";
 
-export default function ViewCam() { 
-    return (
-        <div className="flex justify-center items-center m-9 p-9">
-            <Webcam />
-        </div>
-    )
+export default function ViewCam() {
+  const webcamRef = React.useRef(null);
+  const [imgSrc, setImgSrc] = React.useState([]);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc((prev) => [...prev, imageSrc]);
+  }, [webcamRef, setImgSrc]);
+
+  return (
+    <div className="flex flex-col justify-center items-center m-9 p-9">
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        width={900}
+        screenshotFormat="image/jpeg"
+      />
+
+      <button
+        className="bg-[aqua] p-5 m-5 rounded-xl"
+        onClick={() => setInterval(capture, 2000)}
+      >
+        Capture photo
+      </button>
+
+      <div className="grid grid-cols-5 overflow-auto">
+        {imgSrc.map((img) => {
+          return <img className="w-20 h-20 m-5" src={img} />;
+        })}
+      </div>
+    </div>
+  );
 }
