aba8803a247bb85d2eb457f8b98416c7d668526e
Abdelrahman Mohamed <80596612+AbdElrahmanBEBO@users.noreply.github.com>
2022-05-16 09:59:50 +0200

    l

--- src/Pages/Camera/ViewCam.jsx
+++ src/Pages/Camera/ViewCam.jsx
@@ -5,30 +5,35 @@ export default function ViewCam() {
   const webcamRef = React.useRef(null);
   const [imgSrc, setImgSrc] = React.useState([]);
 
-  const capture = React.useCallback(() => {
+  const capture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc((prev) => [...prev, imageSrc]);
-  }, [webcamRef, setImgSrc]);
+  }
+
+  let date = new Date();
+  let sec = date.getSeconds();
+
 
   return (
-    <div className="flex flex-col justify-center items-center m-9 p-9">
+    <div className="flex flex-col justify-center items-center m-5">
       <Webcam
+        className="rounded-2xl shadow-lg shadow-black"
         audio={false}
         ref={webcamRef}
-        width={900}
-        screenshotFormat="image/jpeg"
+        width={1024}
+        screenshotFormat="image/png"
       />
 
       <button
-        className="bg-[aqua] p-5 m-5 rounded-xl"
-        onClick={() => setInterval(capture, 2000)}
+        className="bg-black text-white p-3 m-5 rounded-xl font-medium"
+        onClick={() => setInterval(capture, 1500)}
       >
         Capture photo
       </button>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 overflow-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 overflow-auto">
         {imgSrc.map((img) => {
-          return <img className="w-20 h-20 m-5" src={img} />;
+          return <img className="w-[200px] h-[200px] m-5" src={img} />;
         })}
       </div>
     </div>
